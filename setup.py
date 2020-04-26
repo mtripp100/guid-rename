c88c3d708e2115af5bc7aba0dbe36577f4ba7a37
Magnus Tripp <mtripp100@gmail.com>
2020-04-26 12:03:17 +0100

    Add tox.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
+
 setup(
-    name='guid-rename',
-    version='0.1',
+    name="guid-rename",
+    version="0.1",
     packages=find_packages(),
+    install_requires=["click"],
+    entry_points={"console_scripts": {"guid_rename = guid_rename.__main__:run"}},
 )
